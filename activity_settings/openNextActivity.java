9ee4414e92d518acea69fd1621a8880666634ed1
Vedat Gezme <kartacagrafiker@gmail.com>
2020-10-05 10:15:39 +0300

    Create openNextActivity.java

---
+++ activity_settings/openNextActivity.java
@@ -0,0 +1,8 @@
+
+// Just add a function or onclick method so you'll go to next activity
+
+Intent intent = new Intent(this,write_your_next_activity_name_here.class);
+                startActivity(intent);
+                finish();
+
+
